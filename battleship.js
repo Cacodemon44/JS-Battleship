08b73731c5f6ca169c1c2edefc3658a062297db8
Cacodemon44 <alexey.gatin78@gmail.com>
2023-09-20 12:12:07 +0300

    New updates

--- battleship.js
+++ battleship.js
@@ -1,23 +1,38 @@
-var location1=3
-var location2=4
-var location3=5
+
+var randomLoc=Math.floor(Math.random()*5); // возращает рандомное  значения от 0 до 1. Умножаем на 5, чтобы получить числа от 0,5 до 4,999
+// Math.floor откругляет в меньшую сторону.
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits=0;
 var guesses=0;
 var isSunk=false;
-while isSunk==false; //пока корабль не потоплен
-    guess=prompt("Ready,aim,fire!..please enter a number from 0 to 6:") //запрашиваем данные у пользвателя
-    if (guess<0 || guess >6) {
-        alert("please enter  number from 0 to 6"); } 
-        else { 
-        guesses=guesses+1;
-        }
-        if guess= location1 or location2 or location3 {
-            hits=hits+1
-        }
-            if hits=3 {
+while (isSunk==false)
+    { //пока корабль не потоплен
+        guess=prompt("Ready,aim,fire!..please enter a number from 0 to 6:"); //запрашиваем данные у пользвателя
+    if (guess <0 || guess >6) 
+        { //проверяем предположение пользователя
+        alert("please enter  number from 0 to 6"); }
+        else 
+        { 
+        guesses=guesses+1; // если цифры в диапазоне - добавляется значение к попыткам
+            if (guess == location1 || guess == location2 || guess ==location3) 
+        {
+                   alert("HIT!");
+                   hits=hits+1; //если пользователь попал, добавляется значение к попаданиям    
+                if (hits==3) {
                 isSunk=true;
-                alert ("The ship is destroyed");
+                alert ("The ship is destroyed");} 
+        }
+            else {
+                   alert ("MISS"); 
             }
-
+        } 
+     }
+var stats = "You took " + guesses + " guesses to sink the battleship, " + 
+"which means your shooting accuracy was " + (3/guesses); // Обратная связь. Вводится новая переменная,
+// сообщающая пользователю информацию о факте его действий
+alert(stats); //выводится значение данной переменной
         
+
--- code.js
+++ code.js
@@ -1,13 +1,24 @@
-var word = "bottles";
-var count = 99;
-while (count > 0) {
- console.log(count + " " + word + " of beer on the wall");
- console.log(count + " " + word + " of beer,");
- console.log("Take one down, pass it around,");
- count = count - 1;
- if (count > 0) {
- console.log(count + " " + word + " of beer on the wall.");
- } else {
- console.log("No more " + word + " of beerfs on the wall.");
- }
-}
+function bark(name, weight) {
+    if (weight > 20) {
+    console.log(name + " says WOOF WOOF");
+    } else {
+    console.log(name + " says woof woof");
+    }
+   }
+bark("rover", 23);
+bark("spot", 13);
+bark("spike", 53);
+bark("lady", 17);
+
+function whatShallIWear (temp) {
+    if (temp<60) {
+        console.log ("Wear a jacket");}
+          else if (temp<70) {
+                console.log ("Wear a sweater") }
+                else { 
+            console.log ("Wear t-shirt"); }
+                     
+        }
+whatShallIWear (50);
+whatShallIWear (80);    
+whatShallIWear (60);
